9a0bc6d1172e743b5899cb5e6763406617766ec5
unknown <phantomnam0116@outlook.com>
2019-05-08 19:51:16 +0530

    make it better

--- src/apis/Profile.js
+++ src/apis/Profile.js
@@ -38,7 +38,7 @@ export function edituserprofile(user_id,username,useremail,fullName,gender,DOB,r
 
 
 export function verfyMoNo(otp,user_id) {
-  return axios.post(config.ApiUrl+'users/mobile',{otp:mobile,user_id:user_id});
+  return axios.post(config.ApiUrl+'users/mobile',{otp:otp,user_id:user_id});
 };
 
 export function verfyotp(otp,user_id) {
